c3c8a53089ce022006c59286903c07e56a995b72
Nico-0711 <nicocastro436@gmail.com>
2024-06-26 19:09:22 -0300

    Nicolás Castro - 308251

---
+++ entrega.sh
@@ -0,0 +1,43 @@
+
+mostrar_menu() {
+    echo "============================="
+    echo "      Menú de Opciones       "
+    echo "============================="
+    echo "1. Opción 1"
+    echo "2. Opción 2"
+    echo "3. Opción 3"
+    echo "4. Salir"
+    echo "============================="
+}
+
+
+ejecutar_opcion() {
+    case $1 in
+        1)
+            echo "Tocaste el 1"
+           
+            ;;
+        2)
+            echo "Tocaste el 2"
+          
+            ;;
+        3)
+            echo "Tocaste el 3"
+         
+            ;;
+        4)
+            echo "Saliendo..."
+            exit 0
+            ;;
+        *)
+            echo "Tocaste mal"
+            ;;
+    esac
+}
+
+# Bucle principal del menú
+while true; do
+    mostrar_menu
+    read -p "Selecciona una opción: " opcion
+    ejecutar_opcion $opcion
+done
